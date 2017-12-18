512b677d714ac7b028576aa98b14c96843bcd94f
young <young.java@qq.com>
2017-12-18 13:09:48 +0800

    clear

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@
 public class Main {
     //test
     public static void main(String[] args) {
-        //test2
-        System.out.println("hello, world!");
+        System.out.println("hello, world!");        
     }
 }
